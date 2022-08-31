a161284a71ab0a818802d76b906d829334b60c40
Aleksander Szczepaniak <lequit7@gmail.com>
2022-08-31 17:08:28 +0200

    Removing items from the cart

--- components/layout/CartMenu.js
+++ components/layout/CartMenu.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Backdrop from './Backdrop'
 import { useSelector, useDispatch } from 'react-redux'
-import { AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineClose, AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 import { toggleCartMenu } from '../../redux/uiSlice'
 import { AnimatePresence, motion } from 'framer-motion'
+import { removeItem } from '../../redux/cartSlice'
 
 const CartMenu = () => {
     const { items: cartItems } = useSelector(state => state.cart);
@@ -24,6 +25,7 @@ const CartMenu = () => {
                         animate={{ x: 0 }}
                         exit={{ x: '100vw' }}
                         transition={{ type: 'tween' }}
+                        onClick={(e) => e.stopPropagation()}
                         className='z-50 fixed right-0 sm:w-[35vw] w-full h-screen flex flex-col justify-between p-4 bg-white'>
                         <div>
                             {/* Heading */}
@@ -37,11 +39,22 @@ const CartMenu = () => {
                             <div className='mt-8 lex flex-col gap-4'>
                                 {cartItems.map(item => (
                                     <div className='flex justify-between'>
-                                        <div>
-                                            <h3 className='text-lg'>{item.title}</h3>
-                                            <p className='font-light'>{item.ingredients}</p>
+                                        <div className='flex items-center gap-x-4'>
+                                            <div className='flex items-center gap-x-2 text-gray-500'>
+                                                <button onClick={() => {
+                                                    dispatch(removeItem(item))
+                                                }}><AiOutlineClose /></button>
+
+                                            </div>
+                                            <div>
+                                                <h3 className='text-lg'>{item.title}</h3>
+                                                <p className='font-light'>{item.ingredients}</p>
+                                            </div>
                                         </div>
-                                        <p>{item.price} zł</p>
+
+                                        <div className='flex items-center gap-x-1'>
+                                            <p>{item.price} zł</p>
+                                        </div>
                                     </div>
                                 ))}
                             </div>
--- redux/cartSlice.js
+++ redux/cartSlice.js
@@ -8,13 +8,27 @@ export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    /**
+     * Adds item to the cart
+     * @param {Object} action - item to be added
+     */
     addItem: (state, action) => {
       state.items.push(action.payload);
     },
+    /**
+     * Removes an item from the cart
+     * @param {Object} action - item to be removed
+     */
+    removeItem: (state, action) => {
+      const index = state.items.findIndex(item => item.id === action.payload.id);
+      if (index > -1) {
+        state.items.splice(index, 1);
+      }
+    }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { addItem } = cartSlice.actions;
+export const { addItem, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
