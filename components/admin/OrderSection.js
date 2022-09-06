de2a0eb7438d480da023eb4daafb12b0ae0a7dfc
Aleksander Szczepaniak <lequit7@gmail.com>
2022-09-06 13:19:07 +0200

    Showing reservations in the admin panel

--- components/admin/OrderSection.js
+++ components/admin/OrderSection.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { getPriceSum } from '../../utils/cart-utils';
 
 const OrderSection = () => {
@@ -20,42 +20,47 @@ const OrderSection = () => {
 
     if (loading) return <div>Ładowanie...</div>
 
-    if(!orders) return <div>Brak zamówień</div>
+    if (!orders) return <div>Brak zamówień</div>
 
     return (
-        <div className='overflow-x-auto relative'>
-            <table className='w-full text-sm text-center text-gray-500 dark:text-gray-400'>
-                <thead className='"text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"'>
-                    <tr>
-                        <th scope='col' className='py-3 px-6'>
-                            Numer telefonu
-                        </th>
-                        <th scope='col' className='py-3 px-6'>
-                            Adres
-                        </th>
-                        <th scope='col' className='py-3 px-6'>
-                            Zamówienie
-                        </th>
-                        <th scope='col' className='py-3 px-6'>
-                            Kwota
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {orders.map(order => (
-                        <tr key={order.id}>
-                            <td scope='row' className='py-3 px-6'>{order.phone_number}</td>
-                            <td scope='row' className='py-3 px-6'>{order.address.street} {order.address.local}</td>
-                            <td scope='row' className='py-3 px-6'>{order.items.map(item => <div key={item._id}>
-                                {item.item.title} x{item.amount}
-                            </div>)}</td>
-                            <td scope='row' className='py-3 px-6'>{getPriceSum(order.items)} zł</td>
-                        </tr>
-                    ))}
+        <Fragment>
+            <h1 className='text-3xl mb-4'>Zamówienia</h1>
 
-                </tbody>
-            </table>
-        </div>
+            <div className='overflow-x-auto relative'>
+                <table className='w-full text-sm text-center text-gray-500 dark:text-gray-400'>
+                    <thead className='"text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"'>
+                        <tr>
+                            <th scope='col' className='py-3 px-6'>
+                                Numer telefonu
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Adres
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Zamówienie
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Kwota
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map(order => (
+                            <tr key={order.id}>
+                                <td scope='row' className='py-3 px-6'>{order.phone_number}</td>
+                                <td scope='row' className='py-3 px-6'>{order.address.street} {order.address.local}</td>
+                                <td scope='row' className='py-3 px-6'>{order.items.map(item => <div key={item._id}>
+                                    {item.item.title} x{item.amount}
+                                </div>)}</td>
+                                <td scope='row' className='py-3 px-6'>{getPriceSum(order.items)} zł</td>
+                            </tr>
+                        ))}
+
+                    </tbody>
+                </table>
+            </div>
+        </Fragment>
+
     )
 }
 
--- components/admin/ReservationSection.js
+++ components/admin/ReservationSection.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import { getPriceSum } from '../../utils/cart-utils';
 
 const ReservationSection = () => {
+    const [loading, setLoading] = useState(true);
+    const [reservations, setReservations] = useState();
+
+    useEffect(() => {
+        const fetchReservations = async () => {
+            setLoading(true);
+            const res = await fetch('/api/reservation');
+
+            const resData = await res.json();
+
+            setReservations(resData.reservations);
+            setLoading(false);
+        }
+        fetchReservations();
+    }, []);
+
+    if (loading) return <div>Ładowanie...</div>
+
+    if (!reservations) return <div>Brak rezerwacji</div>
+
     return (
+        <Fragment>
+            <h1 className='text-3xl mb-4'>Rezerwacje</h1>
 
-        <div class="overflow-x-auto relative">
+            <div className='overflow-x-auto relative'>
+                <table className='w-full text-sm text-center text-gray-500 dark:text-gray-400'>
+                    <thead className='"text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"'>
+                        <tr>
+                            <th scope='col' className='py-3 px-6'>
+                                Data i godzina
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Imię i nazwisko
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Numer telefonu
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Email
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Liczba osób
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Notatka
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {reservations.map(reservation => (
+                            <tr key={reservation.id}>
+                                <td scope='row' className='py-3 px-6'>{reservation.day} <br /> {reservation.hour}</td>
+                                <td scope='row' className='py-3 px-6'>{reservation.firstname} {reservation.lastname}</td>
+                                <td scope='row' className='py-3 px-6'>{reservation.phone}</td>
+                                <td scope='row' className='py-3 px-6'>{reservation.email}</td>
+                                <td scope='row' className='py-3 px-6'>{reservation.num_people}</td>
+                                <td scope='row' className='py-3 px-6'>{reservation.note || '-'}</td>
+                            </tr>
+                        ))}
 
-        </div>
+                    </tbody>
+                </table>
+            </div>
+        </Fragment>
 
     )
 }
